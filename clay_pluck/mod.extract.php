122ed6da8efc8d4033af964b4c9a1a4c789b10b9
Jack McDade <jack@jackmcdade.com>
2014-06-27 10:23:45 -0400

    prefix modifiers with `clay_` to avoid 1st-party collision and add ability to set markers with a config file.

---
+++ clay_pluck/mod.extract.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Modifier_pluck
+ * returns portion of string between defined markers marker_start and marker_end
+ * characters used for markers are tricky - some get parsed by either slim or php and send it to the moon
+ * the ones below DO work, and seem to make some sense
+ * @author  Clay Harmon
+ *   
+ */
+class Modifier_extract extends Modifier
+{
+    public function index($value, $parameters=array())
+    {
+        $marker_start = array_get($this->config, 'marker_start', '[=');
+        $marker_end = array_get($this->config, 'marker_end', '=]');
+        $more_string = array_get($this->config, 'more_string', '&rArr;');
+
+        //test for existence of markers in string and just return original if false
+        if (   ($beg = strpos($value, $marker_start) + strlen($marker_start))
+            && ($end = strpos($value, $marker_end))) {
+        
+            $length = $end  - $beg;
+
+            return substr($value, $beg, $length) . $more_string;
+    	}
+    	
+        return $value;
+    }
+}
